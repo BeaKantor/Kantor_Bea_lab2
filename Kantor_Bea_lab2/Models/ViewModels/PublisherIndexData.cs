4abb914ba799e137c2d69b62e05fadecae277282
Beata Kantor <beata.roberta11@gmail.com>
2024-10-28 15:30:51 +0200

    Inceput Lab4

---
+++ Kantor_Bea_lab2/Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Kantor_Bea_lab2.Models;
+
+namespace Kantor_Bea_lab2.Models.ViewModels
+
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }  
+        public IEnumerable<Book> Books { get; set; }
+     }
+}
