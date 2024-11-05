39a468313cdbf1b9f909ad71a2ddfb9deea6ebe1
Beata Kantor <beata.roberta11@gmail.com>
2024-11-05 18:21:04 +0200

    retusari lab4

--- Kantor_Bea_lab2/Pages/Categories/Index.cshtml.cs
+++ Kantor_Bea_lab2/Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Kantor_Bea_lab2.Data;
 using Kantor_Bea_lab2.Models;
+using Kantor_Bea_lab2.Models.ViewModels;
 
 namespace Kantor_Bea_lab2.Pages.Categories
 {
@@ -21,9 +22,31 @@ namespace Kantor_Bea_lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public List<Book> AllBooks { get; set; } = new List<Book>();
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories).ThenInclude(i => i.Book)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+
+            AllBooks = await _context
+                .Book.Include(b => b.Author)
+                .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(b => b.Book).ToList();
+            }
         }
     }
 }
